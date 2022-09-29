a1b02b381dc25c9376bfa6080f246acf3609ec00
Alexandra <matildakrokodilda@gmail.com>
2022-09-29 12:10:34 +0300

    Привет, Маша

---
+++ Example005_HelloMasha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите ваше имя");
+string username = Console.ReadLine ();
+if (username.ToLower()=="вася")
+{
+    Console.WriteLine ("Славабогутыпришел");
+}
+
+else
+{
+    Console.Write ("Снова ты,");
+    Console.WriteLine (username);
+}
