066f0fed8c0d9bf50c85a31de45c2f887e828431
Alexandra <matildakrokodilda@gmail.com>
2022-09-29 12:42:37 +0300

    findMax

---
+++ Example006_findMax/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1 = 24;
+int number2 = 6;
+int number3 = 1;
+int number4 = 24;
+int number5 = 1;
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+if (number4 > max) max = number4;
+if (number5 > max) max = number5;
+Console.Write ("max = ");
+Console.WriteLine (max);
